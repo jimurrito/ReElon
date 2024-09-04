783c80bb37a5431f6a2307eb7f049918e1e1500f
jimurrito <jimurrito@gmail.com>
2024-09-04 04:04:26 +0000

    Cleaned up repo

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # ReElon
 
+Changes a certain CEO's profile picture on twitter to that of his photo from the paypal buyout.
+
+## How to install
+
+### Firefox
+1. Goto [about:debugging](about:debugging) in firefox.
+2. Select the `This Firefox` option on the left.
+3. Under `Temporary Extensions`, select `Load Temporary Add-on...`
+4. In the pop-out window, navigate to the `manifest.json` file of this extension and select it.
+
+## Chromium (Chrome, Edge, Brave)
+1. Goto [chrome://extensions](chrome://extensions) in your chromium based browser.
+2. Select the `Developer Mode` toggle button.
+3. Find and select the `Load unpacked` option.
+4. In the pop-out window, navigate to the root directory containing the `manifest.json` file and select it.
+
 ## How to test?
 
-1. Go to https://twitter.com/elonmusk
+1. Go to https://twitter.com/elonmusk and see for your self!
--- content.js
+++ content.js
@@ -1,19 +1,25 @@
-//document.body.innerHTML = document.body.innerHTML.replace('https://pbs.twimg.com/profile_images/1815749056821346304/jS8I28PL_200x200.jpg', 'https://pbs.twimg.com/profile_images/1831143048719597568/k5yM2fKQ_400x400.jpg');
-//setTimeout(15000)
-
-
 var new_pfp = 'https://pbs.twimg.com/profile_images/1831143048719597568/k5yM2fKQ_400x400.jpg';
-var replace_pfp = [
-    'https://pbs.twimg.com/profile_images/1815749056821346304/jS8I28PL_200x200.jpg',
-    'https://pbs.twimg.com/profile_images/1815749056821346304/jS8I28PL_400x400.jpg',
-    'https://pbs.twimg.com/profile_images/1815749056821346304/jS8I28PL_normal.jpg',
+var old_pfps_all = [
+    /[A-Za-z]+:\/\/pbs\.twimg\.com\/profile_images\/[A-Za-z0-9]+\/[A-Za-z0-9]+_200x200\.jpg/g,
+    /[A-Za-z]+:\/\/pbs\.twimg\.com\/profile_images\/[A-Za-z0-9]+\/[A-Za-z0-9]+_400x400\.jpg/g,
+    /[A-Za-z]+:\/\/pbs\.twimg\.com\/profile_images\/[A-Za-z0-9]+\/[A-Za-z0-9]+_normal\.jpg/g
 ];
-
-for (var i in replace_pfp) {
-    document.body.innerHTML = document.body.innerHTML.replace(replace_pfp[i], new_pfp);
-    document.body.outerHTML = document.body.outerHTML.replace(replace_pfp[i], new_pfp);
-    console.log(replace_pfp[i])
+var old_pfps_je = [
+    /[A-Za-z]+:\/\/pbs\.twimg\.com\/profile_images\/1815749056821346304+\/jS8I28PL+_200x200\.jpg/g,
+    /[A-Za-z]+:\/\/pbs\.twimg\.com\/profile_images\/1815749056821346304+\/jS8I28PL+_400x400\.jpg/g,
+    /[A-Za-z]+:\/\/pbs\.twimg\.com\/profile_images\/1815749056821346304+\/jS8I28PL+_normal\.jpg/g
+];
+const sleep = (ms = 0) => new Promise(resolve => setTimeout(resolve, ms));
+//
+async function replaceElon(new_pfp, old_pfps) {
+    //
+    await sleep(2000);
+    //
+    for (var i in old_pfps) {
+        document.body.innerHTML = document.body.innerHTML.replace(old_pfps[i], new_pfp);
+        document.body.outerHTML = document.body.outerHTML.replace(old_pfps[i], new_pfp);
+    }
+//
 }
-
-
-
+//
+replaceElon(new_pfp, old_pfps_je);
